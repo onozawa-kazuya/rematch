c7ad6fb0283263722aa88b0559bfc768441f23a2
”onozawa-kazuya” <kazuya.jpee@gmail.com>
2020-08-01 14:14:35 +0000

    memory change

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -58,7 +58,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
+                    {{ __('messages.Logout') }}
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
                     @csrf
